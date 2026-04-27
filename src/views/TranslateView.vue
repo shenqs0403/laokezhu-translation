b382d2084a145d622773aff1430b9e80d9a1de48
johnson <shenqs0403@163.com>
2026-04-27 13:39:35 +0800

    翻译界面，失去焦点关闭在调试完成后启用

--- src/views/TranslateView.vue
+++ src/views/TranslateView.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import {onMounted, ref} from "vue";
+import {invoke} from "@tauri-apps/api/core";
+import {getCurrentWindow} from "@tauri-apps/api/window";
 
+const targetText = ref("");
+
+onMounted(() => {
+  let currentWindow = getCurrentWindow();
+  // currentWindow.once("tauri://blur",() => {
+  //   currentWindow.close();
+  // });
+
+  invoke<string>("translate_selected_text",{engineName: "",lang: ""}).then(val => {
+    console.log(val);
+    targetText.value = val;
+  }).catch(e => alert(e));
+})
 </script>
 
 <template>
-翻译界面
+{{ targetText }}
 </template>
 
 <style scoped>
