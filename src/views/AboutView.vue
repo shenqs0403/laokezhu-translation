cd3b349b689607a46ffa4ee0910b19bd395c2d43
johnson <shenqs0403@163.com>
2026-04-28 16:56:13 +0800

    去除轮询功能

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -12,8 +12,6 @@ import {version} from "../../package.json";
   名称：自定义，随便写<br>
   命令： laokezhu-translation -- translate<br>
   快捷键： 自定义，设置成自己喜欢的<br>
-  <h3>当前版本已知问题</h3>
-  轮询菜单需要先点击一下才能失去焦点关闭
 </template>
 
 <style scoped>
